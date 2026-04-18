29d9f8df4e6260d9c13e84fa542f464e736c553e
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2026-04-18 14:40:16 +0200

    Create logger.py

---
+++ utils/logger.py
@@ -0,0 +1,25 @@
+---
+
+# 📄 3. `utils/logger.py`
+
+```python
+import logging
+import sys
+
+def get_logger(name: str = "APP"):
+
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+
+    if not logger.handlers:
+
+        handler = logging.StreamHandler(sys.stdout)
+
+        formatter = logging.Formatter(
+            "[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s"
+        )
+
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+    return logger
