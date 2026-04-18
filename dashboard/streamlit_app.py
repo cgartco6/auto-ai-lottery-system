a435d236051d3766f22fa3a3ee00c433c7f04995
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2026-04-18 21:36:12 +0200

    Update streamlit_app.py

--- dashboard/streamlit_app.py
+++ dashboard/streamlit_app.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 import streamlit as st
 
 st.set_page_config(page_title="AI Simulation Dashboard")
