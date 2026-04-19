fdeac5a59de85bf5799239311772cd9aad023dab
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2026-04-19 15:38:43 +0200

    Update streamlit_app.py

--- dashboard/streamlit_app.py
+++ dashboard/streamlit_app.py
@@ -1,12 +1,28 @@
 import streamlit as st
+import pandas as pd
+
 from pipeline.generate_predictions import generate
 
-st.title("PowerBall AI Prediction Dashboard")
+st.title("AI PowerBall Prediction Dashboard")
 
 top13, top6 = generate()
 
-st.subheader("Top 6 Strongest Predictions")
-st.write(top6)
 
-st.subheader("Top 13 Prediction Pool")
-st.write(top13)
+st.header("Top 6 Strongest Predictions")
+
+df6 = pd.DataFrame(
+    [list(x[0]) + [x[1]] for x in top6],
+    columns=["N1","N2","N3","N4","N5","Score"]
+)
+
+st.dataframe(df6)
+
+
+st.header("Top 13 Prediction Pool")
+
+df13 = pd.DataFrame(
+    [list(x[0]) + [x[1]] for x in top13],
+    columns=["N1","N2","N3","N4","N5","Score"]
+)
+
+st.dataframe(df13)
